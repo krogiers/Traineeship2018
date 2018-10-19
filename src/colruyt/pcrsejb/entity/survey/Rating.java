3fcf960a6308f862e619ba3d934d0a7b9ad52377
xavier de aguirre <xavier_deaguirre002@hotmail.com>
2018-10-19 11:18:45 +0200

    Rating class fuctions v3

--- src/colruyt/pcrsejb/entity/survey/Rating.java
+++ src/colruyt/pcrsejb/entity/survey/Rating.java
@@ -11,6 +11,9 @@ public class Rating {
     private Competence competence;
     
     
+    
+    
+    /** Constructs a new Rating. */
     public Rating(){
     }
     
@@ -20,30 +23,61 @@ public class Rating {
         this.competence = competence;
     }
     
+    /**
+     * Getter for property 'level'.
+     *
+     * @return Value for property 'level'.
+     */
     public int getLevel(){
         return level;
     }
     
+    /**
+     * Setter for property 'level'.
+     *
+     * @param level Value to set for property 'level'.
+     */
     public void setLevel(int level){
         this.level = level;
     }
     
+    /**
+     * Getter for property 'energy'.
+     *
+     * @return Value for property 'energy'.
+     */
     public boolean isEnergy(){
         return energy;
     }
     
+    /**
+     * Setter for property 'energy'.
+     *
+     * @param energy Value to set for property 'energy'.
+     */
     public void setEnergy(boolean energy){
         this.energy = energy;
     }
     
+    /**
+     * Getter for property 'competence'.
+     *
+     * @return Value for property 'competence'.
+     */
     public Competence getCompetence(){
         return competence;
     }
     
+    /**
+     * Setter for property 'competence'.
+     *
+     * @param competence Value to set for property 'competence'.
+     */
     public void setCompetence(Competence competence){
         this.competence = competence;
     }
     
+    /** {@inheritDoc} */
     @Override
     public boolean equals(Object o){
         if(this == o)
@@ -54,11 +88,13 @@ public class Rating {
         return getLevel() == rating.getLevel() && isEnergy() == rating.isEnergy() && Objects.equals(getCompetence(), rating.getCompetence());
     }
     
+    /** {@inheritDoc} */
     @Override
     public int hashCode(){
         return Objects.hash(getLevel(), isEnergy(), getCompetence());
     }
     
+    /** {@inheritDoc} */
     @Override
     public String toString(){
         return "Rating{" + "level=" + level + ", energy=" + energy + ", competence=" + competence + '}';

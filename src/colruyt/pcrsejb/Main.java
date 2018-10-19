c7ea3b7f758aa296f6d2faa2a9376ac6fc48055d
Yendel Lintermans <Yendel.lintermans@outlook.be>
2018-10-19 08:54:24 +0200

    Cleaning file.

--- src/colruyt/pcrsejb/Main.java
+++ src/colruyt/pcrsejb/Main.java
@@ -4,8 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        //Yendel Test
         System.out.println("HEllo World");
-        //TODO COMMENTAARTEST DOOR LAURA
     }
 }

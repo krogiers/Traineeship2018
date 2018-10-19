638d9d38993e25e78fb9003108e9c06a8965cc2f
bvvyq47 <bvvyq47@WS459577.admin.colruyt.int>
2018-10-19 10:42:55 +0200

    Begin DataLinkLaag , Competences

---
+++ src/colruyt/pcrsejb/dl/competence/MemoryCompetenceService.java
@@ -0,0 +1,39 @@
+package colruyt.pcrsejb.dl.competence;
+
+import java.util.List;
+
+import colruyt.pcrsejb.entity.competence.Competence;
+
+public class MemoryCompetenceService implements AbstractCompetenceService {
+
+	@Override
+	public void addElement(Competence element) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Competence getElement(Long index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Competence> getAllElements() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteElement(Competence element) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Competence element) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}

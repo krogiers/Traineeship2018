078dd337053683ebad7886cc287483ef6f6d3d15
bvvyq47 <bvvyq47@WS459577.admin.colruyt.int>
2018-10-19 11:04:27 +0200

    Update Generic Repo

--- src/colruyt/pcrsejb/dl/competence/MemoryCompetenceService.java
+++ src/colruyt/pcrsejb/dl/competence/MemoryCompetenceService.java
@@ -1,39 +1,38 @@
 package colruyt.pcrsejb.dl.competence;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import colruyt.pcrsejb.entity.competence.Competence;
 
 public class MemoryCompetenceService implements AbstractCompetenceService {
 
+	private List<Competence> db = new ArrayList<>();
+	
 	@Override
 	public void addElement(Competence element) {
-		// TODO Auto-generated method stub
+		db.add(element);
 		
 	}
 
 	@Override
 	public Competence getElement(Long index) {
-		// TODO Auto-generated method stub
-		return null;
+		return this.db.get((index.intValue()));
 	}
 
 	@Override
 	public List<Competence> getAllElements() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return this.db;
 	}
 
 	@Override
 	public void deleteElement(Competence element) {
-		// TODO Auto-generated method stub
+		this.db.remove(element);
 		
 	}
 
-	@Override
-	public void update(Competence element) {
-		// TODO Auto-generated method stub
-		
-	}
+	
 
 }

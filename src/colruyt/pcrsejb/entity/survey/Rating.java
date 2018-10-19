ed6d32f36fd88ad6ae39800151df22b5d1618ff4
xavier de aguirre <xavier_deaguirre002@hotmail.com>
2018-10-19 12:52:50 +0200

    Comments to rating and comsensusrating V1

--- src/colruyt/pcrsejb/entity/survey/Rating.java
+++ src/colruyt/pcrsejb/entity/survey/Rating.java
@@ -4,6 +4,8 @@ import colruyt.pcrsejb.entity.competence.Competence;
 
 import java.util.*;
 
+/**
+ The type Rating. */
 public class Rating {
 
     private int level;
@@ -11,67 +13,68 @@ public class Rating {
     private Competence competence;
     
     
-    
-    
-    /** Constructs a new Rating. */
+    /**
+     Constructs a new Rating.
+     */
     public Rating(){
     }
     
+    
+    /**
+     Instantiates a new Rating.
+     @param level the level
+     @param energy the energy
+     @param competence the competence
+     */
     public Rating(int level, boolean energy, Competence competence){
-        this.level = level;
-        this.energy = energy;
-        this.competence = competence;
+        setLevel(level);
+        setEnergy(energy);
+        setCompetence(competence);
     }
     
     /**
-     * Getter for property 'level'.
-     *
-     * @return Value for property 'level'.
+     Getter for property 'level'.
+     @return Value for property 'level'.
      */
     public int getLevel(){
         return level;
     }
     
     /**
-     * Setter for property 'level'.
-     *
-     * @param level Value to set for property 'level'.
+     Setter for property 'level'.
+     @param level Value to set for property 'level'.
      */
     public void setLevel(int level){
         this.level = level;
     }
     
     /**
-     * Getter for property 'energy'.
-     *
-     * @return Value for property 'energy'.
+     Getter for property 'energy'.
+     @return Value for property 'energy'.
      */
     public boolean isEnergy(){
         return energy;
     }
     
     /**
-     * Setter for property 'energy'.
-     *
-     * @param energy Value to set for property 'energy'.
+     Setter for property 'energy'.
+     @param energy Value to set for property 'energy'.
      */
     public void setEnergy(boolean energy){
         this.energy = energy;
     }
     
     /**
-     * Getter for property 'competence'.
-     *
-     * @return Value for property 'competence'.
+     Getter for property 'competence'.
+     @return Value for property 'competence'.
      */
     public Competence getCompetence(){
         return competence;
     }
     
     /**
-     * Setter for property 'competence'.
-     *
-     * @param competence Value to set for property 'competence'.
+     Setter for property 'competence'.
+     @param competence Value to set for property 'competence'.
      */
     public void setCompetence(Competence competence){
         this.competence = competence;

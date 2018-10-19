848f98e8ad73962a5e57d7f6c09f1495cdda7292
xavier de aguirre <xavier_deaguirre002@hotmail.com>
2018-10-19 10:24:45 +0200

    Rating class fuctions v1

--- src/colruyt/pcrsejb/entity/survey/Rating.java
+++ src/colruyt/pcrsejb/entity/survey/Rating.java
@@ -7,5 +7,38 @@ public class Rating {
     private int level;
     private boolean energy;
     private Competence competence;
-
+    
+    
+    public Rating(){
+    }
+    
+    public Rating(int level, boolean energy, Competence competence){
+        this.level = level;
+        this.energy = energy;
+        this.competence = competence;
+    }
+    
+    public int getLevel(){
+        return level;
+    }
+    
+    public void setLevel(int level){
+        this.level = level;
+    }
+    
+    public boolean isEnergy(){
+        return energy;
+    }
+    
+    public void setEnergy(boolean energy){
+        this.energy = energy;
+    }
+    
+    public Competence getCompetence(){
+        return competence;
+    }
+    
+    public void setCompetence(Competence competence){
+        this.competence = competence;
+    }
 }

718f8865c7457e165625c806bc1eae38a29a607a
Yendel Lintermans <Yendel.lintermans@outlook.be>
2018-10-19 09:57:48 +0200

    Adding TestClass

---
+++ src/colruyt/pcrsejb/test/TestClass.java
@@ -0,0 +1,55 @@
+package colruyt.pcrsejb.test;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestClass {
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    void createUser(){
+
+    }
+
+    //Add user to team
+
+    //3 users in a team, get owner
+
+    //user, get all privileges
+
+    //user, get function
+
+    //function, get all roles
+
+    //craftcompetence of those roles
+
+    //give me all competences from a function
+
+    //check if there are only functioncompetences
+
+    //check if you can rate a competence
+
+    //consensusrate a competence --> add comment
+
+    //create survey
+
+    //create surveyset --> 3 surveys
+
+    //can i submit a survey?
+
+    //can i get the responsible function person from the function
+
+    //create Operating Unit
+
+    
+
+
+
+
+}

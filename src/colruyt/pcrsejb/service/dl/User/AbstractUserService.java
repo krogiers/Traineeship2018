484ead978822ff61c2159d16d620b88a00186c04
thomasBillet <thomasgod2@gmail.com>
2018-10-25 10:56:42 +0200

    Rip SmartConverters

--- src/colruyt/pcrsejb/service/dl/User/AbstractUserService.java
+++ src/colruyt/pcrsejb/service/dl/User/AbstractUserService.java
@@ -1,10 +1,14 @@
 package colruyt.pcrsejb.service.dl.User;
 
+import colruyt.pcrsejb.entity.privileges.Privilege;
 import colruyt.pcrsejb.entity.user.User;
 import colruyt.pcrsejb.service.dl.GenericCrudService;
 
+import java.util.List;
+
 public interface AbstractUserService extends GenericCrudService<User,Long>{
 
-
+    List<User> findUsersByPrivilege(Privilege privilege);
+    List<User> findUsersByFirstName(String name);
 
 }

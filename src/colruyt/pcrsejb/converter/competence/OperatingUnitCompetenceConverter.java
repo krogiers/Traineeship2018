d529f7695060aac20db3a11c44cb0fd44455f179
LynnVanderhallen <lynn.vanderhallen@gmail.com>
2018-10-31 16:18:47 +0100

    Made OperatingUnitCompetenceConverterB0 and edits to OperatingUnitCompetenceConverter

--- src/colruyt/pcrsejb/converter/competence/OperatingUnitCompetenceConverter.java
+++ src/colruyt/pcrsejb/converter/competence/OperatingUnitCompetenceConverter.java
@@ -1,12 +1,12 @@
 package colruyt.pcrsejb.converter.competence;
 
-import colruyt.pcrsejb.entity.competence.Competence;
-import colruyt.pcrsejb.entity.competence.OperatingUnitCompetence;
+import colruyt.pcrsejb.bo.competence.CompetenceBo;
 import colruyt.pcrsejb.bo.competence.OperatingUnitCompetenceBo;
+import colruyt.pcrsejb.entity.competence.OperatingUnitCompetence;
 
-public class OperatingUnitCompetenceConverter implements CompetenceBoConverter<OperatingUnitCompetenceBo> {
+public class OperatingUnitCompetenceConverter implements CompetenceConverter<OperatingUnitCompetence> {
     @Override
-    public Competence convertTo(OperatingUnitCompetenceBo from) {
-        return new OperatingUnitCompetence(from.getCompetenceID(), from.getName());
+    public CompetenceBo convertTo(OperatingUnitCompetence from) {
+        return new OperatingUnitCompetenceBo(from.getCompetenceID(), from.getName());
     }
 }

183268e7a4adfda059c895e4010ce6c41d67563a
LPL1QQC <LPL1QQC@WS283126.admin.colruyt.int>
2018-10-31 13:33:18 +0100

    changes in Dbcompetenceservice

--- src/colruyt/pcrsejb/service/dl/competence/DbCompetenceService.java
+++ src/colruyt/pcrsejb/service/dl/competence/DbCompetenceService.java
@@ -1,18 +1,27 @@
 package colruyt.pcrsejb.service.dl.competence;
 
 import colruyt.pcrsejb.entity.competence.Competence;
+
 import colruyt.pcrsejb.entity.competence.CraftCompetence;
 import colruyt.pcrsejb.entity.competence.FunctionCompetence;
 import colruyt.pcrsejb.service.dl.DbService;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 public class DbCompetenceService extends DbService implements CompetenceService{
 
 
     @Override
-    public Collection<FunctionCompetence> findAllFunctionCompetences() {
-        return null;
+    public Collection<FunctionCompetence> findAllFunctionCompetences()
+    {
+    	return null;
     }
 
     @Override
@@ -32,9 +41,28 @@ public class DbCompetenceService extends DbService implements CompetenceService{
 
     @Override
     public Collection<Competence> getAllElements() {
+        //TODO: afwerken
+    	List<Competence> competences = new ArrayList<>(); 
+        try(Connection conn = this.createConnection())
+        {
+        	PreparedStatement statement = conn.prepareStatement("Select * from competences");
+        	ResultSet rs = statement.executeQuery();
+        	//competences = rs.convertToCompetenceList();
+        }
+        catch(SQLException e)
+        {
+        	e.printStackTrace();
+        }
         return null;
     }
 
+    
+    private List<Competence> convertToCompetenceList(ResultSet rs)
+    {
+    	//TODO
+    	return null;
+    	
+    }
     @Override
     public void deleteElement(Competence element) {
 

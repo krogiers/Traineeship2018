e0c8c16dd315bb2f36134b2276fb95749ed88566
jda1mbw <jda1mbw@WS368489.admin.colruyt.int>
2018-11-02 16:32:21 +0100

    .

--- src/colruyt/pcrsejb/service/dl/role/DbRoleService.java
+++ src/colruyt/pcrsejb/service/dl/role/DbRoleService.java
@@ -1,8 +1,36 @@
 package colruyt.pcrsejb.service.dl.role;
 
+import java.util.Collection;
+
+import colruyt.pcrsejb.entity.role.Role;
 import colruyt.pcrsejb.service.dl.DbService;
+import colruyt.pcrsejb.service.dl.competence.CompetenceService;
 
-public class DbRoleService  extends DbService {
+public class DbRoleService  extends DbService implements RoleService{
 
+	@Override
+	public Role save(Role element) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
+	@Override
+	public Role getElement(Role element) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<Role> getAllElements() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteElement(Role element) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	//TODO
 }

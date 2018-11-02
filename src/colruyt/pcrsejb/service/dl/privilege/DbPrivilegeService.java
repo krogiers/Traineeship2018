09f5982b53aaa47c66edf368d9a0292bc34e78d9
Yendel Lintermans <Yendel.lintermans@outlook.be>
2018-11-02 12:07:58 +0100

    Updated DbPrivilegeService --Still needs to be corrected!

--- src/colruyt/pcrsejb/service/dl/privilege/DbPrivilegeService.java
+++ src/colruyt/pcrsejb/service/dl/privilege/DbPrivilegeService.java
@@ -15,70 +15,49 @@ import java.util.List;
 
 public class DbPrivilegeService  extends DbService implements PrivilegeService  {
 
-    private static final String ADD_PRIVILEGE = "INSERT into privis (id,shortname,fullname) values ((select max(id) from privis)+1),?,?)";
+    private static final String ADD_PRIVILEGE = "INSERT into privis (id, fullname, shortname) values ((select max(id) from privis)+1,?,?)";
+    private static final String UPDATE_PRIVILEGE = "INSERT into privis (id, fullname, shortname) values (((select max(id) from privis)+1), ?, ?)";
+    private static final String GET_ELEMENT = "Select  * from privis where id = ?";
+    private static final String GET_ALL_ELEMENTS = "Select * from privis";
+    private static final String DELETE_ELEMENT = "Delete from privis where id = ?" ;
 
 
     @Override
     public Privilege save(Privilege element) {
 
         try(Connection conn = this.createConnection()){
+            PreparedStatement statement;
 
-            PreparedStatement statement =  conn.prepareStatement(ADD_PRIVILEGE);
+            if(element.getId() != null){
+                statement = conn.prepareStatement(UPDATE_PRIVILEGE, new String[] {"ID"});
+            }
+            else{
+                statement = conn.prepareStatement(ADD_PRIVILEGE, new String[] {"ID"});
+            }
 
-            statement.setLong(1,element.getId());
-            statement.setString(2,this.reverseTypeing(element) + "");
-            statement.setString(3,element.getClass().getSimpleName());
-
+            statement.setString(1,""); //fullname
+            statement.setString(2, ""); //shortname
 
             statement.executeUpdate();
             ResultSet rs = statement.getGeneratedKeys();
 
             if(rs.next()) {
-                element.setId((int) rs.getLong(1));
-                return element;
-            }
-            else {
-                throw new IllegalArgumentException();
+                element.setId(rs.getInt("ID"));
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        //TODO: Standaarden
-        return null;
-
+        return element;
     }
 
-
-
-
-    private char reverseTypeing(Privilege privi){
-
-            char e = 'V';
-            switch(privi.getClass().getSimpleName()){
-
-                case "TeamManagerPrivilege" : e = 'M';break;
-                case "TeamMemberPrivilege" : e = 'T';break;
-                case "AdminPrivilege" : e = 'A';break;
-                case "FunctionResponsiblePrivilege" : e='F'; break;
-                case  "DirectorPrivilege" : e = 'D'; break;
-            }
-
-            return e;
-
-
-    }
 
     @Override
-    //TODO: Fix --> moet Index E werden (Char van privilege)
     public Privilege getElement(Privilege privilege) {
         try(Connection conn = this.createConnection()){
-
-            PreparedStatement statement =  conn.prepareStatement("Select  * from privis where id = ?");
+            PreparedStatement statement =  conn.prepareStatement(GET_ELEMENT);
             statement.setInt(1,privilege.getId());
             ResultSet rs =  statement.executeQuery();
             privilege = convertToSinglePrivilege(rs);
-
-
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -90,7 +69,7 @@ public class DbPrivilegeService  extends DbService implements PrivilegeService  
         List<Privilege> privileges = new ArrayList<>();
         try(Connection conn = this.createConnection()){
 
-            PreparedStatement statement =  conn.prepareStatement("Select * from privis");
+            PreparedStatement statement =  conn.prepareStatement(GET_ALL_ELEMENTS);
 
             ResultSet rs =  statement.executeQuery();
             privileges = convertToPrivilegeList(rs);
@@ -105,23 +84,23 @@ public class DbPrivilegeService  extends DbService implements PrivilegeService  
     public void deleteElement(Privilege element) {
 
         try(Connection conn = this.createConnection()){
-
-            PreparedStatement statement =  conn.prepareStatement("Delete from privis where id = ?");
+            PreparedStatement statement =  conn.prepareStatement(DELETE_ELEMENT);
             statement.setLong(1,element.getId());
-            ResultSet rs =  statement.executeQuery();
-
+            statement.executeQuery();
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
     }
+
+
+
+    //TODO BEKIJK DEZE METHODS
 
 
 
 
 
     private List<Privilege> convertToPrivilegeList(ResultSet set) throws SQLException {
-
         List<Privilege> privileges = new ArrayList<>();
         while(set.next()){
             Privilege p;
@@ -132,7 +111,6 @@ public class DbPrivilegeService  extends DbService implements PrivilegeService  
             privileges.add(p);
         }
         return privileges;
-
     }
 
     private Privilege determineInstance(char type){
@@ -147,21 +125,31 @@ public class DbPrivilegeService  extends DbService implements PrivilegeService  
         }
 
         return p;
-
     }
 
+    //    private char reverseTypeing(Privilege privi){
+//
+//            char e = 'V';
+//            switch(privi.getClass().getSimpleName()){
+//
+//                case "TeamManagerPrivilege" : e = 'M';break;
+//                case "TeamMemberPrivilege" : e = 'T';break;
+//                case "AdminPrivilege" : e = 'A';break;
+//                case "FunctionResponsiblePrivilege" : e='F'; break;
+//                case  "DirectorPrivilege" : e = 'D'; break;
+//            }
+//
+//            return e;
+//    }
 
     private Privilege convertToSinglePrivilege(ResultSet set) throws SQLException {
-
         Privilege p = null;
         if(set.next()){
-
             char privilege = set.getString("shortname").charAt(0);
             p = determineInstance(privilege);
             p.setId(set.getInt("ID"));
 
         }
-
         return p;
     }
 }

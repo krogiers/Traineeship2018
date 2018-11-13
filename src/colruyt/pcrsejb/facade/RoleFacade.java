e9ff96e8499777ac796432b9ed460f514920d515
svroye <vanroyesteven@gmail.com>
2018-11-13 14:00:01 +0100

    Role services

--- src/colruyt/pcrsejb/facade/RoleFacade.java
+++ src/colruyt/pcrsejb/facade/RoleFacade.java
@@ -1,12 +1,26 @@
 package colruyt.pcrsejb.facade;
 
+import colruyt.pcrsejb.bo.role.RoleBo;
 import colruyt.pcrsejb.converter.role.RoleBoConverter;
 import colruyt.pcrsejb.converter.role.RoleConverter;
+import colruyt.pcrsejb.entity.role.Role;
 import colruyt.pcrsejb.service.bl.RoleServiceBL;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class RoleFacade {
 
 	private RoleServiceBL roleServiceBL= new RoleServiceBL();
 	private RoleBoConverter roleBoConverter = new RoleBoConverter();
 	private RoleConverter roleConverter = new RoleConverter();
+
+
+	public List<RoleBo> getAllRoles(){
+		List<RoleBo> roleBoList = new ArrayList<>();
+		for (Role role : roleServiceBL.getAllRoles()){
+			roleBoList.add(roleConverter.convertTo(role));
+		}
+		return roleBoList;
+	}
 }
--- src/colruyt/pcrsejb/service/bl/RoleServiceBL.java
+++ src/colruyt/pcrsejb/service/bl/RoleServiceBL.java
@@ -1,10 +1,16 @@
 package colruyt.pcrsejb.service.bl;
 
+import colruyt.pcrsejb.entity.role.Role;
 import colruyt.pcrsejb.service.dl.role.DbRoleService;
 import colruyt.pcrsejb.service.dl.role.RoleService;
+
+import java.util.List;
 
 public class RoleServiceBL {
 
 	private RoleService roledb = new DbRoleService();
-	
+
+    public List<Role> getAllRoles() {
+        return (List<Role>) roledb.getAllElements();
+    }
 }

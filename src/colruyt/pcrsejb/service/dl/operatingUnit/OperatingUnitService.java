903e2353f656b619b96bdadb9cd8260fdfa6e68b
Jente sondervorst <jentesondervorst@gmail.com>
2018-11-13 09:14:50 +0100

    converter error

---
+++ src/colruyt/pcrsejb/service/dl/operatingUnit/OperatingUnitService.java
@@ -0,0 +1,8 @@
+package colruyt.pcrsejb.service.dl.operatingUnit;
+
+import colruyt.pcrsejb.entity.operatingunit.OperatingUnit;
+import colruyt.pcrsejb.service.dl.GenericCrudService;
+
+public interface OperatingUnitService extends GenericCrudService<OperatingUnit,Integer> {
+
+}
